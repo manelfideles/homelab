9c771291f391e71464b9bf07aae5b0a8435de195
Manuel Fideles <manuel.fideles@redlightsoft.com>
2026-02-02 11:17:24 +0000

    setup fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p ~/homelab/{config/{jellyfin,immich,n8n,radarr,prowlarr,qbittorrent,vaultwarden,bazarr,unmanic,sonarr},data/{movies,tvshows,photos,downloads,vault},cache/unmanic}
+mkdir -p ./{config/{jellyfin,immich,n8n,radarr,prowlarr,qbittorrent,vaultwarden,bazarr,unmanic,sonarr},data/{movies,tvshows,photos,downloads,vault},cache/unmanic}
 
-sudo chown -R $USER:$USER ~/homelab
-chmod -R 755 ~/homelab
+sudo chown -R $USER:$USER .
+chmod -R 777 .
 
 echo "✅ Directories ready for the full suite."
